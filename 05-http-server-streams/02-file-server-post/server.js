d60c79c634cea49ccb0c5c1e4bd06c52eb57e81d
Karpenko.nataliia <netaly79@gmail.com>
2021-05-29 23:26:02 +0300

    Task 5-2

--- 05-http-server-streams/02-file-server-post/server.js
+++ 05-http-server-streams/02-file-server-post/server.js
@@ -1,6 +1,8 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const Lst = require('./LimitSizeStream');
 
 const server = new http.Server();
 
@@ -11,9 +13,40 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'POST':
+      if (path.dirname(req.url) === '/') {
+        if (fs.existsSync(filepath)) {
+          res.statusCode = 409;
+          res.end();
+        } else {
+          const wst = fs.createWriteStream(filepath, {flags: 'wx'});
+          const limitS = new Lst({limit: 1048576});
+          req.pipe(limitS)
+              .on('error', (e) => {
+                fs.unlink(filepath, ()=>{});
+                res.statusCode = 413;
+                res.end(e.message);
+              })
+              .pipe(wst)
+              .on('error', (e) => {
+                res.statusCode = 500;
+                res.end(e.message);
+              });
 
+          wst.on('finish', () => {
+            res.statusCode = 201;
+            res.end('success');
+          });
+
+          req.on('aborted', () => {
+            fs.unlink(filepath, ()=>{});
+            wst.destroy();
+          });
+        }
+      } else {
+        res.statusCode = 400;
+        res.end();
+      }
       break;
-
     default:
       res.statusCode = 501;
       res.end('Not implemented');
