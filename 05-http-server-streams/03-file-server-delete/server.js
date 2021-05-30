e641b1173af2c53074d5dfeea0348e98ea6dfad0
Karpenko.nataliia <netaly79@gmail.com>
2021-05-30 15:30:10 +0300

    Task 5-3

--- 05-http-server-streams/03-file-server-delete/server.js
+++ 05-http-server-streams/03-file-server-delete/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -11,6 +12,26 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'DELETE':
+      if (path.dirname(req.url) === '/') {
+        try {
+          if (!fs.existsSync(filepath)) {
+            res.statusCode = 404;
+            res.end();
+          } else {
+            fs.unlink(filepath, () => {
+              console.log(`${filepath} was deleted`);
+            });
+            res.statusCode = 200;
+            res.end('success');
+          }
+        } catch (e) {
+          res.statusCode = 500;
+          res.end(e.message);
+        }
+      } else {
+        res.statusCode = 400;
+        res.end();
+      }
 
       break;
 
