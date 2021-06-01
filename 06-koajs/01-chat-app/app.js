c642a1de2a5d1c51cb8f96ea34ef509113953871
Karpenko.nataliia <netaly79@gmail.com>
2021-06-01 20:26:55 +0300

    Task 6

--- 06-koajs/01-chat-app/app.js
+++ 06-koajs/01-chat-app/app.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const Koa = require('koa');
+const {EventEmitter} = require('events');
 const app = new Koa();
 
 app.use(require('koa-static')(path.join(__dirname, 'public')));
@@ -7,11 +8,24 @@ app.use(require('koa-bodyparser')());
 
 const Router = require('koa-router');
 const router = new Router();
+const postEmitter = new EventEmitter();
 
 router.get('/subscribe', async (ctx, next) => {
+  await new Promise((resolve) => {
+    postEmitter.once('post', (message) => {
+      resolve(message);
+    });
+  }).then((message) => {
+    ctx.body = message;
+  });
 });
 
 router.post('/publish', async (ctx, next) => {
+  const message = ctx.request.body.message;
+  if (message && message !== '') {
+    postEmitter.emit('post', message);
+    ctx.response.status = 200;
+  }
 });
 
 app.use(router.routes());
